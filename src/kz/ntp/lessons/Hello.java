a33eecbbbeb64430c4da6fe75b0dc56aac05d5c6
Alihser Tlb <alisher97_kz@mail.ru>
2017-10-04 22:03:46 +0600

    Sys.out.print-озгерісі

--- src/kz/ntp/lessons/Hello.java
+++ src/kz/ntp/lessons/Hello.java
@@ -3,5 +3,7 @@ package kz.ntp.lessons;
 public class Hello {
     public static void main(String[] args){
         System.out.println("Hello world!");
+        System.out.println("Бұл зертханалық жұмыс");
+        System.out.println("GitHub-пен жұмыс жасау.");
     }
 }
